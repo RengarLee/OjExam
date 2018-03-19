ea6f9cd6b9fc4f592a09c277c28d5e6f1695e76f
RengarLee <RengarLee@163.com>
2018-03-19 16:35:03 +0800

    无新内容 Dal层bug为修复

---
+++ OjExam.BLL/BaseService.cs
@@ -0,0 +1,49 @@
+﻿using OjExam.DALFactory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OjExam.BLL
+{
+    public class BaseService<T> where T:class, new ()
+    {
+    //    public DbSession 
+
+    //    #region Qurry
+    //    public T GetEntities(Expression<Func<T, bool>> whereLamdba)
+    //    {
+    //        //return db.Entry.GetEntities(whereLamdba);
+    //        return db.Set<T>().Where<T>(whereLamdba).FirstOrDefault();
+    //    }
+
+    //    public IQueryable<T> GetPageEntities<S>(int pageIndex, int pageSize, Expression<Func<T, bool>> whereLamdba, Expression<Func<T, S>> orderByLamdba, bool isAsc)
+    //    {
+    //        return db.Set<T>().Where<T>(whereLamdba).Skip<T>((pageIndex - 1) * pageSize).Take<T>(pageSize).OrderBy<T, S>(orderByLamdba) as IQueryable<T>;
+    //    }
+    //    #endregion
+
+    //    #region Add Delete Updata
+    //    public bool Add(T entity)
+    //    {
+    //        db.Set<T>().Add(entity);
+    //        return true;
+    //    }
+
+    //    public bool Delete(T entity)
+    //    {
+    //        db.Entry<T>(entity).State = System.Data.Entity.EntityState.Deleted;
+    //        return true;
+    //    }
+
+    //    public bool Updata(T entity)
+    //    {
+    //        db.Entry<T>(entity).State = System.Data.Entity.EntityState.Modified;
+    //        return true;
+    //    }
+    //    #endregion
+    //}
+}
+}
