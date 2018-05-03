8101c2a8dcec409b321ff3791e0ea78a8f77b39b
RengarLee <RengarLee@163.com>
2018-05-03 16:02:16 +0800

    基础完成

--- OjExam.UIPortal/App_Start/BundleConfig.cs
+++ OjExam.UIPortal/App_Start/BundleConfig.cs
@@ -14,8 +14,6 @@ namespace OjExam.UIPortal
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            // 使用要用于开发和学习的 Modernizr 的开发版本。然后，当你做好
-            // 生产准备就绪，请使用 https://modernizr.com 上的生成工具仅选择所需的测试。
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
@@ -26,6 +24,12 @@ namespace OjExam.UIPortal
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            //css
+            bundles.Add(new StyleBundle("~/Content/Layui/css").Include("~/Content/layui-admin/css/style.css", "~/Content/layui-admin/layui/css/layui.css"));
+
+            //js
+            bundles.Add(new ScriptBundle("~/Content/Layui/js").Include("~/Content/layui-admin/js/notification.js", "~/Content/layui-admin/layui/layui.js"));
         }
     }
 }
---
+++ OjExam.UIPortal/Content/layui-admin/README.md
@@ -0,0 +1,15 @@
+# 基于Layuiv2.2.45做的后台模板  
+作者:[Dust](http://dust101.lofter.com)  
+-----
+## 特色:
+ * 基于Layuiv2.2.45，绝不同于百度搜索到的同类Layui后台模板的layuiv1版本
+ * 单页面 Tab标签式
+ * 可以从收藏夹里打开指定的Tab
+ * Tab切换或打开带有渐变的视觉特效
+ * 简单，快捷，复制index.html出来引用Layui即可。
+ * 以后更新...
+
+## 截图:
+![1](https://github.com/mumbaicat/layui-admin/raw/master/screenshots/1.png)  
+![2](https://github.com/mumbaicat/layui-admin/raw/master/screenshots/2.png)  
+![3](https://github.com/mumbaicat/layui-admin/raw/master/screenshots/3.png)  
